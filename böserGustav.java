d26e015b843bbd6b0ec00a2cf3d6744229ab4072
Athion2001 <aaron.kaspa@posteo.de>
2026-08-04 02:12:44 +0200

    böserGsutav

---
+++ böserGustav.java
@@ -0,0 +1,12 @@
+public class böserGustav{
+    boolean gustavIstNochNichtDa;
+    public static void main(String[] args){
+        böserGustav bG = new böserGustav();
+    }
+    public böserGustav(){
+        gustavIstNochNichtDa = false;
+        while(!gustavIstNochNichtDa){
+            System.out.println("böser Gustav!");
+        }
+    }
+}
