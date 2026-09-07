06ebd1c65303a96bbc73b8aca7f636f9a23cd8b5
Athion2001 <aaron.kaspar@posteo.de>
2026-09-07 13:35:43 +0200

    1

--- IchBinVerlinked.java
+++ IchBinVerlinked.java
@@ -2,4 +2,7 @@ public class IchBinVerlinked {
     public static void main(String[] args){
         System.out.println("ich bin verlinked hihi und es hat geklappt");
     }
+    public void huhu(){
+        
+    }
 }
