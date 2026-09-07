8ed8018d8551c55c5fe9baeb4fa9e36fecfe9142
Athion2001 <aaron.kaspar@posteo.de>
2026-09-07 14:10:49 +0200

    huhu

--- IchBinVerlinked.java
+++ IchBinVerlinked.java
@@ -1,8 +1,21 @@
+import java.awt.Desktop;
+import java.io.File;
 public class IchBinVerlinked {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("ich bin verlinked hihi und (schwul) es hat geklappt");
     }
-    public void huhu(){
+    public void huhu() throws Exception {
+            File bild = new File(
+            "H:\\Pictures\\Screenshots\\Screenshot 2026-09-07 140321.png"
+        );
 
-    }
+        while (true) {
+            Desktop.getDesktop().open(bild);
+
+            Thread.sleep(2000); // 2 Sekunden warten
+        }
+    
+
 }
+}
+
